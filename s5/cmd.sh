c02277264f5a552e8e500264ab71f21511bb0338
marcomarini93 <marcomarini93@gmail.com>
2020-12-17 11:40:11 +0100

    it has been modified cmd.sh and path.sh in order to be run it in Xerses. in run just some changes to make it more readable

--- s5/cmd.sh
+++ s5/cmd.sh
@@ -1,15 +1,17 @@
 # Setting local system jobs (local CPU - no external clusters)
-#export train_cmd=run.pl
-#export decode_cmd=run.pl
-#export cuda_cmd=run.pl
-#export mkgraph_cmd=run.pl
+export train_cmd=run.pl
+export decode_cmd=run.pl
+export cuda_cmd=run.pl
+export mkgraph_cmd=run.pl
 
-memval=4G
+
+# Setting external clusters system jobs
+#memval=4G
 #h_rt="08:00:00"
 #mem="-l mem=$mem_val,h_rt=$h_rt"
 
-export train_cmd="queue.pl --mem $memval"
-export decode_cmd="queue.pl --mem $memval"
-export mkgraph_cmd="queue.pl --mem $memval"
-export cuda_cmd="queue.pl --mem $memval --gpu 1"
-export cmd="queue.pl --mem $memval"
+#export train_cmd="queue.pl --mem $memval"
+#export decode_cmd="queue.pl --mem $memval"
+#export mkgraph_cmd="queue.pl --mem $memval"
+#export cuda_cmd="queue.pl --mem $memval --gpu 1"
+#export cmd="queue.pl --mem $memval"
